c86e8a30d95e32ca7a5a79aaa4d45fc2fecba2f3
Artem Lobovskii <lobovskiy@yandex.ru>
2023-05-14 13:10:54 +0300

    feat: add screen size initializing and styles constants for screen sizes

--- minesweeper/src/constants/index.js
+++ minesweeper/src/constants/index.js
@@ -4,16 +4,44 @@ const SCREEN_SIZES = {
   desktop: 'desktop',
 };
 
-const SCREEN_SIZES_MIN_WIDTH = {
+const SCREEN_SIZES_MIN_WIDTHS = {
   [SCREEN_SIZES.mobile]: 500,
   [SCREEN_SIZES.tablet]: 768,
   [SCREEN_SIZES.desktop]: 992,
 };
 
+const SCREEN_SIZES_ASCENDING = Object.keys(SCREEN_SIZES_MIN_WIDTHS).sort(
+  (a, b) => SCREEN_SIZES_MIN_WIDTHS[a] - SCREEN_SIZES_MIN_WIDTHS[b],
+);
+
 const DIFFICULTY = {
   beginner: 'beginner',
   intermediate: 'intermediate',
   expert: 'expert',
+};
+
+const STYLES_BY_SCREEN_SIZE = {
+  [SCREEN_SIZES.mobile]: {
+    CELL_SIZE_BY_DIFFICULTY: {
+      [DIFFICULTY.beginner]: '50px',
+      [DIFFICULTY.intermediate]: '40px',
+      [DIFFICULTY.expert]: '20px',
+    },
+  },
+  [SCREEN_SIZES.tablet]: {
+    CELL_SIZE_BY_DIFFICULTY: {
+      [DIFFICULTY.beginner]: '50px',
+      [DIFFICULTY.intermediate]: '40px',
+      [DIFFICULTY.expert]: '20px',
+    },
+  },
+  [SCREEN_SIZES.desktop]: {
+    CELL_SIZE_BY_DIFFICULTY: {
+      [DIFFICULTY.beginner]: '50px',
+      [DIFFICULTY.intermediate]: '40px',
+      [DIFFICULTY.expert]: '20px',
+    },
+  },
 };
 
 const MINEFIELD_SIZE = {
@@ -31,4 +59,11 @@ const MINEFIELD_SIZE = {
   },
 };
 
-export { SCREEN_SIZES, SCREEN_SIZES_MIN_WIDTH, DIFFICULTY, MINEFIELD_SIZE };
+export {
+  SCREEN_SIZES,
+  SCREEN_SIZES_MIN_WIDTHS,
+  SCREEN_SIZES_ASCENDING,
+  DIFFICULTY,
+  STYLES_BY_SCREEN_SIZE,
+  MINEFIELD_SIZE,
+};
