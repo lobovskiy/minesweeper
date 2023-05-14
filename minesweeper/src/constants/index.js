fc4f0510bdbe792f1f81ef72fc470e6857c0c0fc
Artem Lobovskii <lobovskiy@yandex.ru>
2023-05-14 13:23:54 +0300

    feat: remove screen size initializing

--- minesweeper/src/constants/index.js
+++ minesweeper/src/constants/index.js
@@ -1,19 +1,3 @@
-const SCREEN_SIZES = {
-  mobile: 'mobile',
-  tablet: 'tablet',
-  desktop: 'desktop',
-};
-
-const SCREEN_SIZES_MIN_WIDTHS = {
-  [SCREEN_SIZES.mobile]: 500,
-  [SCREEN_SIZES.tablet]: 768,
-  [SCREEN_SIZES.desktop]: 992,
-};
-
-const SCREEN_SIZES_ASCENDING = Object.keys(SCREEN_SIZES_MIN_WIDTHS).sort(
-  (a, b) => SCREEN_SIZES_MIN_WIDTHS[a] - SCREEN_SIZES_MIN_WIDTHS[b],
-);
-
 const DIFFICULTY = {
   beginner: 'beginner',
   intermediate: 'intermediate',
@@ -35,10 +19,4 @@ const MINEFIELD_SIZE_BY_DIFFICULTY = {
   },
 };
 
-export {
-  SCREEN_SIZES,
-  SCREEN_SIZES_MIN_WIDTHS,
-  SCREEN_SIZES_ASCENDING,
-  DIFFICULTY,
-  MINEFIELD_SIZE_BY_DIFFICULTY,
-};
+export { DIFFICULTY, MINEFIELD_SIZE_BY_DIFFICULTY };
--- minesweeper/src/services/settings.js
+++ minesweeper/src/services/settings.js
@@ -1,32 +1,10 @@
-import {
-  SCREEN_SIZES,
-  SCREEN_SIZES_MIN_WIDTHS,
-  SCREEN_SIZES_ASCENDING,
-  DIFFICULTY,
-  MINEFIELD_SIZE_BY_DIFFICULTY,
-} from '../constants';
+import { DIFFICULTY, MINEFIELD_SIZE_BY_DIFFICULTY } from '../constants';
 
 class Settings {
   constructor() {
     this.difficulty = DIFFICULTY.beginner;
     this.boardSize = MINEFIELD_SIZE_BY_DIFFICULTY[this.difficulty];
     this.sound = true;
-    this.screenSize = undefined;
-
-    this.initScreenSize();
-  }
-
-  initScreenSize() {
-    const windowWidth = window.innerWidth;
-    let screenSize = SCREEN_SIZES.mobile;
-
-    SCREEN_SIZES_ASCENDING.forEach((size) => {
-      if (windowWidth > SCREEN_SIZES_MIN_WIDTHS[size]) {
-        screenSize = size;
-      }
-    });
-
-    this.screenSize = screenSize;
   }
 
   setDifficulty(value) {
