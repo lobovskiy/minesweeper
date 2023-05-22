b101a78d9f0c3315af187ed80d1cbf934064953a
Artem Lobovskii <lobovskiy@yandex.ru>
2023-05-22 22:35:47 +0300

    feat: set up dropdown styles and animation

--- minesweeper/src/view/components/dropdown/dropdown.js
+++ minesweeper/src/view/components/dropdown/dropdown.js
@@ -1,40 +1,39 @@
+import renderButton from '../button/button';
 import './style.scss';
 
 function renderDropdown(selectorLabel, callbackSelectListItem, listItems = []) {
   const dropdown = document.createElement('div');
   dropdown.classList.add('dropdown');
 
-  const dropdownSelector = document.createElement('button');
+  function toggleDropdown() {
+    if (dropdown.hasAttribute('data-opened')) {
+      dropdown.removeAttribute('data-opened');
+    } else {
+      dropdown.setAttribute('data-opened', '');
+    }
+  }
+
+  const dropdownSelector = renderButton(selectorLabel, () => toggleDropdown());
   dropdownSelector.classList.add('dropdown__selector');
-  dropdownSelector.innerHTML = selectorLabel;
 
   const dropdownList = document.createElement('div');
   dropdownList.classList.add('dropdown__list');
 
-  function toggleDropdownListVisibility() {
-    if (dropdownList.hasAttribute('data-visible')) {
-      dropdownList.removeAttribute('data-visible');
-    } else {
-      dropdownList.setAttribute('data-visible', '');
-    }
-  }
-
   listItems.forEach((listItem) => {
     const item = document.createElement('div');
+    item.classList.add('dropdown__list-item');
     item.innerHTML = listItem;
-    item.addEventListener('click', () => {
-      toggleDropdownListVisibility();
+    item.addEventListener('click', (event) => {
+      event.stopPropagation();
+      toggleDropdown();
       callbackSelectListItem(listItem);
     });
 
     dropdownList.append(item);
   });
 
-  dropdownSelector.addEventListener('click', () =>
-    toggleDropdownListVisibility(),
-  );
-
-  dropdown.append(dropdownSelector, dropdownList);
+  dropdownSelector.append(dropdownList);
+  dropdown.append(dropdownSelector);
 
   return dropdown;
 }
